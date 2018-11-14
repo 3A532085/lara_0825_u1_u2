2df58193848002288b21a90caa4464165b417604
江珮妤 <Luxgen077@gmail.com>
2018-11-14 13:30:57 +0800

    練習3-3:查詢資料 , 使用 find 方法

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,12 @@ Route::get('/', function () {
     $post ->content ='test content';
     $post ->save();
 
-    $posts=\App\Post::all();
-    dd($posts);
+//  $posts=\App\Post::all();
+//  dd($posts);
+
+    $post=\App\Post::find(1);
+    dd($post);
+
 
 
 });
