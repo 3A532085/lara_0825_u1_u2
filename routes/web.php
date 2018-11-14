3aede7f669cb2037ae70d9ee93679c58c4828bca
江珮妤 <Luxgen077@gmail.com>
2018-11-14 12:17:01 +0800

    練習 2-1,2-2：有Mass Assignment 警告

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    \App\Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
+
+
 });
