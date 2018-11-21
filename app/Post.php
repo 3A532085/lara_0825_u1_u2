837defddc8859f73b605fed5f9ed279d54d9dea1
江珮妤 <Luxgen077@gmail.com>
2018-11-21 19:17:16 +0800

    練習7-3:開啟 Post.php , 增加關聯

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\ Comment;
 
 class Post extends Model
 {
@@ -14,6 +15,11 @@ class Post extends Model
         'content',
         'is_feature'
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
 }
 
