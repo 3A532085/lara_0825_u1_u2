4003e4c8017491dc2587a13e6516f54d1ef9fac0
江珮妤 <Luxgen077@gmail.com>
2018-11-28 19:02:55 +0800

    單元二_練習4:調整 seeder 產生的資料

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Post as Post;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -12,11 +14,22 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+        Post::truncate();
+
         foreach(range(1,20) as $number){
+            $total=20;
+            $faker=Faker::create('zh_TW');
+
             Post::create([
-                'title'=>'title'.$number,
-                'content'=>'content'.$number,
+               // 'title'=>'title'.$number,
+               // 'content'=>'content'.$number,
+                'is_feature'=>rand(0,1),
+                'created_at'=>Carbon::now()->subDays($total-$number),
+                'updated_at'=>Carbon::now()->subDays($total-$number),
+                'title'=>$faker->sentence,
+                'content'=>$faker->paragraph,
             ]);
+
         }
     }
 }
